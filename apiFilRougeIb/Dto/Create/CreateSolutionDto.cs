00c7c684657ed78963f98b830c4fb5071dd35f96
Justin Lhotel <justinlhotel@laposte.net>
2021-02-01 10:19:57 +0100

    Fichiers Solution vides

---
+++ apiFilRougeIb/Dto/Create/CreateSolutionDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiFilRougeIb.Dto.Create
+{
+    public class CreateSolutionDto
+    {
+    }
+}
---
+++ apiFilRougeIb/Repositories/SolutionRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiFilRougeIb.Repositories
+{
+    public class SolutionRepository
+    {
+    }
+}
