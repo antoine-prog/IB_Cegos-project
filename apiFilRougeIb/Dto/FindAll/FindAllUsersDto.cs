ed361bb91f76ade089a1b5045dfaa98e21214a55
Antoine CORO <antoine.coro51@gmail.com>
2021-02-01 16:28:40 +0100

    user again

---
+++ apiFilRougeIb/Dto/FindAll/FindAllUsersDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiFilRougeIb.Dto.FindAll
+{
+    public class FindAllUsersDto
+    {
+        public long? IdUser { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Adress { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsCreator { get; set; }
+        public long Level_idLevel { get; set; }
+
+        public FindAllUsersDto(string firstName, string lastName, string username, string adress, string mail, string password, bool isAdmin, bool isCreator, long level_idLevel, long? idUser= null)
+        {
+            IdUser = idUser;
+            FirstName = firstName;
+            LastName = lastName;
+            Username = username;
+            Adress = adress;
+            Mail = mail;
+            Password = password;
+            IsAdmin = isAdmin;
+            IsCreator = isCreator;
+            Level_idLevel = level_idLevel;
+        }
+
+    }
+}
