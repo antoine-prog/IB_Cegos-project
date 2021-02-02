0292a3713b4413b9fa521cbf8927ee420cb1561c
Justin Lhotel <justinlhotel@laposte.net>
2021-02-02 17:13:18 +0100

    IL MANQUAIT LES ACCESSEURS git statusgit status

--- apiFilRougeIb/Dto/FindAll/FindAllUsersJoinUserAnswersDto.cs
+++ apiFilRougeIb/Dto/FindAll/FindAllUsersJoinUserAnswersDto.cs
@@ -7,24 +7,16 @@ namespace apiFilRougeIb.Dto.FindAll
 {
     public class FindAllUsersJoinUserAnswersDto : FindAllUsersDto
     {
-        public string ok;
-        public List<Models.UserAnswer> listUserAnswers;
+
+        public List<Models.UserAnswer> listUserAnswers { get; set; }
         public FindAllUsersJoinUserAnswersDto(FindAllUsersDto user) :
             base(user.FirstName, user.LastName, user.Username, user.Adress, user.Mail, user.Password, user.IsAdmin, user.IsCreator, user.Level_idLevel, user.IdUser)
         {
-            this.ok = "ok";
             this.listUserAnswers = new Repositories.UserRepository(new Utils.QueryBuilder()).FindAllUserAnswers((long)user.IdUser);
-            Console.WriteLine("Dans le constructeur :");
-            this.Affichage();
-        }
+        }    
+        
 
-        public void Affichage()
-        {
-           foreach (Models.UserAnswer u in this.listUserAnswers)
-            {
-                Console.WriteLine(u.ToString());
-            }
-        }
+       
     }
     
 }
