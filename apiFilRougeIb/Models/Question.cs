bbc183387257c5207549449d89b414bf01d4330f
Justin Lhotel <justinlhotel@laposte.net>
2021-02-03 09:25:50 +0100

    Commit change Question

--- apiFilRougeIb/Models/Question.cs
+++ apiFilRougeIb/Models/Question.cs
@@ -7,6 +7,21 @@ namespace apiFilRougeIb.Models
 {
     public class Question
     { 
-        //Placeholder
+        public long? IdQuestion { get; set; }
+        public string Title { get; set; }
+        public long Theme_idTheme { get; set; }
+        public long Level_idLevel { get; set; }
+
+        public Question() { }
+
+        public Question(string title, long theme_idTheme, long level_idLevel, long? idQuestion = null)
+        {
+            IdQuestion = idQuestion;
+            Title = title;
+            Theme_idTheme = theme_idTheme;
+            Level_idLevel = level_idLevel;
+            
+        }
+
     }
 }
