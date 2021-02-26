b76bbc7fd30b0bb0f8acb847cfe71ece92101a4e
amelle Mboyi <annelle2m@yahoo.fr>
2021-02-26 14:35:52 +0100

    home, quiz

--- angularFilRougeIb/src/app/app-routing.module.ts
+++ angularFilRougeIb/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeConnecteComponent } from './_pages/home-connecte/home-connecte.comp
 import { HomeComponent } from './_pages/home/home.component';
 import { InscriptionComponent } from './_pages/inscription/inscription.component';
 import { NotfoundComponent } from './_pages/notfound/notfound.component';
+import { QuizCreeCodeComponent } from './_pages/quiz-cree-code/quiz-cree-code.component';
 
 
 const routes: Routes = [
   {path:"", component : HomeComponent},
   {path:"inscription", component : InscriptionComponent},
   {path:"home-connecte", component : HomeConnecteComponent}, 
+  {path:"quiz-cree-code", component : QuizCreeCodeComponent},
   {path:"404", component : NotfoundComponent},
   {path:"**", redirectTo: '404'}
 ];
